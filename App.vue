f3d2da5dfc30ca6016eaf2e395be8486f5100030
Aichung <aichunghsueh@gmail.com>
2020-09-23 23:15:12 +0800

    add app.vue&node_modules

---
+++ App.vue
@@ -0,0 +1,18 @@
+
+
+
+<script>
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+
+</style>
